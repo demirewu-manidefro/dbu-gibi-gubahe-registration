a653fd3d970a7039d264f9a4859c17176e36b9e7
deme-ctrl <demirewumanidefro@gmail.com>
2026-02-05 14:40:23 -0800

    feat(graduation): add graduation list page

---
+++ frontend/src/pages/GraduationList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StudentList from './StudentList';
+
+const GraduationList = () => {
+    return <StudentList mode="Graduated" />;
+};
+
+export default GraduationList;
