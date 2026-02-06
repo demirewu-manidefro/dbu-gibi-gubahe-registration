52322908c4f25e649183283531b2a9ba4cdf7724
abii16 <danielfekede1616@gmail.com>
2026-02-06 23:28:32 +0300

    feat: Add Ethiopian colleges and departments data

---
+++ frontend/src/utils/collegesData.js
@@ -0,0 +1,64 @@
+// DBU Colleges and Departments Data Structure
+export const collegesAndDepartments = {
+    "College of Freshman": [
+        "Natural Science",
+        "Social Science"
+    ],
+    "College of Computing": [
+        "Computer Science",
+        "Information Systems",
+        "Information Technology",
+        "Software Engineering",
+        "Data Science"
+    ],
+    "College of Engineering": [
+        "Civil Engineering",
+        "Electrical and Computer Engineering",
+        "Mechanical Engineering",
+        "Chemical Engineering",
+        "Construction Technology and Management",
+        "Industrial Engineering",
+        "Food Engineering",
+        "Surveying Engineering"
+    ],
+    "College of Agriculture and Natural Resource Sciences": [
+        "Agricultural Economics",
+        "Animal Science",
+        "Forestry",
+        "Horticulture",
+        "Natural Resource Management",
+        "Plant Science"
+    ],
+    "College of Business and Economics": [
+        "Accounting and Finance",
+        "Economics",
+        "Logistics and Supply Chain Management",
+        "Management",
+        "Marketing Management",
+        "Tourism and Hospitality Management"
+    ],
+    "College of Natural Sciences": [
+        "Biology",
+        "Biotechnology",
+        "Chemistry",
+        "Geology",
+        "Mathematics",
+        "Physics",
+        "Sport Science"
+    ],
+    "College of Social Science and Humanities": [
+        "Amharic Language and Literature",
+        "English Language and Literature",
+        "Geography and Environmental Studies",
+        "History and Heritage Management",
+        "Civics and Ethical Studies",
+        "Psychology",
+        "Sociology",
+        "Journalism and Communications"
+    ],
+    "School of Law": [
+        "Law"
+    ]
+};
+
+export default collegesAndDepartments;
